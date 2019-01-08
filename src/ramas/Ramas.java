c7ba4fb91d8653a3207d4b5bde3bc5d6a9496ef9
dam <dam@172.26.81.176>
2019-01-08 10:03:42 +0100

    Version 1.1

--- src/ramas/Ramas.java
+++ src/ramas/Ramas.java
@@ -5,5 +5,8 @@ public class Ramas {
     public static void main(String[] args) {
         
         System.out.println("Primer cambio");
+        System.out.println("cambios");
+        int a =20;
+        System.out.println(a);
     }
 }
